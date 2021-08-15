b0f782954d6dd1551a3675b3c3d315e02365156f
tf2_mandeokyi <askminjae@gmail.com>
2021-08-15 23:55:02 +0900

    Add @ToString to RGBColorDouble.java

--- src/main/java/com/mndk/tppstlmapper/util/RGBColorDouble.java
+++ src/main/java/com/mndk/tppstlmapper/util/RGBColorDouble.java
@@ -1,11 +1,11 @@
 package com.mndk.tppstlmapper.util;
 
+import lombok.ToString;
+
+@ToString
 public class RGBColorDouble {
 
-
     public final double red, green, blue;
-
-
 
     public RGBColorDouble(double red, double green, double blue) {
         this.red = MathUtils.bound(red, 0, 255);
@@ -13,15 +13,11 @@ public class RGBColorDouble {
         this.blue = MathUtils.bound(blue, 0, 255);
     }
 
-
-
     public RGBColorDouble(int rgb) {
         this.red = (rgb & 0xff0000) >> 16;
         this.green = (rgb & 0x00ff00) >> 8;
         this.blue = rgb & 0x0000ff;
     }
-
-
 
     public int toRGBColor() {
         return (redInt() << 16) + (greenInt() << 8) + blueInt();
